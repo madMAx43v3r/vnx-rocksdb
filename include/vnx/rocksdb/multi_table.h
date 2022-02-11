6b69ea4c6610296594826130c68ad3b58c02f483
Max Wittal <max.wittal@mwittal.de>
2022-02-11 18:06:38 +0100

    find_last()

--- include/vnx/rocksdb/multi_table.h
+++ include/vnx/rocksdb/multi_table.h
@@ -71,12 +71,36 @@ public:
 		iter->Seek(super_t::write(key_stream, key_, super_t::key_type, super_t::key_code));
 		while(iter->Valid()) {
 			super_t::read(iter->key(), key_, super_t::key_type, super_t::key_code);
-			if(mode == EQUAL && key_.first != key) {
+			if(mode == EQUAL && !(key_.first == key)) {
 				break;
 			}
 			values.emplace_back();
 			super_t::read(iter->value(), values.back(), super_t::value_type, super_t::value_code);
 			iter->Next();
+		}
+		return values.size();
+	}
+
+	size_t find_last(const K& key, std::vector<V>& values, const size_t limit) const
+	{
+		values.clear();
+		std::pair<K, I> key_(key, std::numeric_limits<I>::max());
+
+		::rocksdb::ReadOptions options;
+		std::unique_ptr<::rocksdb::Iterator> iter(super_t::db->NewIterator(options));
+
+		typename super_t::stream_t key_stream;
+		iter->SeekForPrev(super_t::write(key_stream, key_, super_t::key_type, super_t::key_code));
+
+		while(iter->Valid() && values.size() < limit)
+		{
+			super_t::read(iter->key(), key_, super_t::key_type, super_t::key_code);
+			if(!(key_.first == key)) {
+				break;
+			}
+			values.emplace_back();
+			super_t::read(iter->value(), values.back(), super_t::value_type, super_t::value_code);
+			iter->Prev();
 		}
 		return values.size();
 	}
@@ -98,7 +122,7 @@ public:
 		while(iter->Valid()) {
 			std::pair<K, I> key_;
 			super_t::read(iter->key(), key_, super_t::key_type, super_t::key_code);
-			if(mode == EQUAL && key_.first != key) {
+			if(mode == EQUAL && !(key_.first == key)) {
 				break;
 			}
 			::rocksdb::WriteOptions options;
